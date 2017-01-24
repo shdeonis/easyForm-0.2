9a0577ba168a0b63f49922b22339a60f62568ba5
shdeonis <shdeonis@gmail.com>
2017-01-24 14:58:24 +0300

    Update module.easyForm.php

--- assets/modules/easyForm/module.easyForm.php
+++ assets/modules/easyForm/module.easyForm.php
@@ -20,27 +20,6 @@ $output=<<<OUT
 <head>
 	<title>Управление формами</title>
 	<link rel="stylesheet" type="text/css" href="media/style/{$eF->theme}/style.css" />
-<style>
-	table{width:100%;}
-	table td{padding:2px 5px !important;border:solid 1px white;height:38px;vertical-align:middle !important;}
-	table thead td{color:white;height:25px;
-		border: 1px solid #658f1a;
-		background: none repeat scroll 0 0 #66901b;
-		text-shadow: 0px -1px 0px #2B5F0C;
-		border-radius:5px 5px 0 0;
-		-moz-border-radius:5px 5px 0 0;
-		-webkit-border-radius:5px 5px 0 0;
-		-ms-border-radius:0;
-		background:-moz-linear-gradient(#8aae4b, #66901b);
-		background:-webkit-gradient(linear, 0 0, 0 100%, from(#8aae4b), to(#66901b));
-		background:-o-linear-gradient(#8aae4b, #66901b);
-	}
-	input[type="text"]{width:300px;margin-bottom:5px !important;}
-	select{width:307px;margin-bottom:5px !important;}
-	input[type="text"].small{width:35px;}
-	p.info{color:#008000;}
-	p.error{color:#cc0000;}
-</style>
 </head>
 <body>
 	<h1>Управление формами</h1>
